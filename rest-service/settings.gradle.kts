1771dbeea28e7ee59dd180d1a23cb25a7bfb1c47
Elizabeth Harper <foxcapade@gmail.com>
2020-10-13 14:46:02 -0400

    Organization

--- rest-service/settings.gradle.kts
+++ rest-service/settings.gradle.kts
@@ -7,8 +7,8 @@ buildProps.load(FileInputStream(File(rootDir, "service.properties")))
 rootProject.name = buildProps.getProperty("project.name")
   ?: error("failed to retrieve project name")
 
-val core = file("../lib-jaxrs-container-core");
-if (core.exists()) {
-  include(":core")
-  project(":core").projectDir = core
-}
+//val core = file("../lib-jaxrs-container-core");
+//if (core.exists()) {
+//  include(":core")
+//  project(":core").projectDir = core
+//}
