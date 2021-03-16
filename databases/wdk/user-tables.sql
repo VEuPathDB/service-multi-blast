3e082137f8b12551cc5c34ba2270324a877bd701
Elizabeth Harper <foxcapade@gmail.com>
2021-03-16 10:44:03 -0400

    update ddl

--- databases/wdk/user-tables.sql
+++ databases/wdk/user-tables.sql
@@ -1,43 +1,43 @@
 CREATE TABLE userlogins5.multiblast_jobs (
   job_digest RAW(16) PRIMARY KEY
-, job_config CLOB           NOT NULL
-, query      CLOB           NOT NULL
-, queue_id   NUMBER(7)
-, project_id VARCHAR2(16)   NOT NULL
-, status     VARCHAR(10)
-, created_on TIMESTAMP      WITH TIME ZONE NOT NULL
-, delete_on  TIMESTAMP      WITH TIME ZONE NOT NULL
+  , job_config CLOB           NOT NULL
+  , query      CLOB           NOT NULL
+  , queue_id   NUMBER(7)
+  , project_id VARCHAR2(16)   NOT NULL
+  , status     VARCHAR(10)
+  , created_on TIMESTAMP      WITH TIME ZONE NOT NULL
+  , delete_on  TIMESTAMP      WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE userlogins5.multiblast_job_to_targets (
   job_digest  RAW(16) NOT NULL REFERENCES userlogins5.multiblast_jobs (job_digest)
-, organism    VARCHAR2(256)
-, target_file VARCHAR2(256)
+  , organism    VARCHAR2(256)
+  , target_file VARCHAR2(256)
 );
 
 CREATE TABLE userlogins5.multiblast_job_to_jobs (
   job_digest    RAW(16) REFERENCES userlogins5.multiblast_jobs (job_digest) NOT NULL
-, parent_digest RAW(16) REFERENCES userlogins5.multiblast_jobs (job_digest) NOT NULL
-, position      NUMBER(4) NOT NULL
-, CONSTRAINT mb_uq_job_job UNIQUE (job_digest, parent_digest)
+  , parent_digest RAW(16) REFERENCES userlogins5.multiblast_jobs (job_digest) NOT NULL
+  , position      NUMBER(4) NOT NULL
+  , CONSTRAINT mb_uq_job_job UNIQUE (job_digest, parent_digest)
 );
 
 CREATE TABLE userlogins5.multiblast_users (
   job_digest        RAW(16)
     REFERENCES userlogins5.multiblast_jobs (job_digest) NOT NULL
-, user_id           NUMBER(12)
+  , user_id           NUMBER(12)
     REFERENCES userlogins5.users (user_id)
-, description       VARCHAR2(1024)
-, max_download_size NUMBER(12) DEFAULT 0 NOT NULL
-, run_directly      NUMBER(1)            NOT NULL
-, CONSTRAINT mb_uq_user_job UNIQUE (job_digest, user_id)
+  , description       VARCHAR2(1024)
+  , max_download_size NUMBER(12) DEFAULT 0 NOT NULL
+  , run_directly      NUMBER(1)            NOT NULL
+  , CONSTRAINT mb_uq_user_job UNIQUE (job_digest, user_id)
 );
 
-GRANT SELECT ON userlogins5.multiblast_jobs TO useraccts_r;
-GRANT SELECT ON userlogins5.multiblast_users TO useraccts_r;
-GRANT SELECT ON userlogins5.multiblast_job_to_jobs TO useraccts_r;
-GRANT SELECT ON userlogins5.multiblast_job_to_targets TO useraccts_r;
-GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_jobs TO useraccts_w;
-GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_users TO useraccts_w;
-GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_job_to_jobs TO useraccts_w;
-GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_job_to_targets TO useraccts_w;
+GRANT SELECT ON userlogins5.multiblast_jobs TO gus_r;
+GRANT SELECT ON userlogins5.multiblast_users TO gus_r;
+GRANT SELECT ON userlogins5.multiblast_job_to_jobs TO gus_r;
+GRANT SELECT ON userlogins5.multiblast_job_to_targets TO gus_r;
+GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_jobs TO gus_w;
+GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_users TO gus_w;
+GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_job_to_jobs TO gus_w;
+GRANT SELECT, INSERT, UPDATE, DELETE ON userlogins5.multiblast_job_to_targets TO gus_w;
