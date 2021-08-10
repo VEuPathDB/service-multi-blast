19f8c8f442f653d96e385382231c0eaa0345bc7f
Elizabeth <foxcapade@gmail.com>
2021-08-10 09:58:45 -0400

    update mblast to use new build tools

--- rest-service/settings.gradle.kts
+++ rest-service/settings.gradle.kts
@@ -13,3 +13,18 @@ rootProject.name = buildProps.getProperty("project.name")
 //  project(":core").projectDir = core
 //}
 include("bom")
+
+pluginManagement {
+  repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+      name = "GitHubPackages"
+      url  = uri("https://maven.pkg.github.com/veupathdb/maven-packages")
+      credentials {
+        username = if (extra.has("gpr.user")) extra["gpr.user"] as String? else System.getenv("GITHUB_USERNAME")
+        password = if (extra.has("gpr.key")) extra["gpr.key"] as String? else System.getenv("GITHUB_TOKEN")
+      }
+    }
+  }
+}
