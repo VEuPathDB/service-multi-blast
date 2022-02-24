2e0845c7347cb90eb78a30ea5cc3f5a7c21a948c
Elizabeth <foxcapade@gmail.com>
2022-02-24 09:59:31 -0500

    separate blast and report queue metric

--- rest-service/src/main/kotlin/mb/lib/report/ReportQueueManager.kt
+++ rest-service/src/main/kotlin/mb/lib/report/ReportQueueManager.kt
@@ -1,15 +1,30 @@
 package mb.lib.report
 
+import io.prometheus.client.Gauge
 import mb.lib.config.Config
 import mb.lib.queue.QueueManager
 import mb.lib.queue.consts.URL
 import mb.lib.report.model.ReportPayload
 import mb.lib.report.model.Request
-import org.apache.logging.log4j.LogManager
 import org.veupathdb.lib.fireworq.FireworqQueue
 
 object ReportQueueManager : QueueManager() {
-  private val log = LogManager.getLogger(ReportQueueManager::class.java)
+
+  private val queueSizeGauge = Gauge.build(
+    "report_queue_size",
+    "Number of jobs currently waiting in the blast queue."
+  ).register()
+
+  init {
+    Thread {
+      while (true) {
+        queueSizeGauge.set(queueSize().toDouble())
+
+        // Sleep 15 seconds
+        Thread.sleep(15_000)
+      }
+    }.start()
+  }
 
   override val fireworq =
     FireworqQueue(URL.prependHTTP(Config.queueHost), Config.formatQueueName)
@@ -21,7 +36,6 @@ object ReportQueueManager : QueueManager() {
   private const val reportEndpoint = "report"
 
   fun submitNewJob(payload: ReportPayload): Int {
-    log.trace("#submitNewJob(payload={})", payload)
     return submitNewJob(Config.formatQueueName, Request(
       java.lang.String.join("/",
         URL.prependHTTP(Config.formatterHost),
