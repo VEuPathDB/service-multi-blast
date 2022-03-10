7c631dee4f2e15422327f408ec6d6b164165b45c
Elizabeth <foxcapade@gmail.com>
2022-03-10 18:20:09 -0500

    correct serialization

--- rest-service/src/main/kotlin/mb/lib/report/model/ReportPayload.kt
+++ rest-service/src/main/kotlin/mb/lib/report/model/ReportPayload.kt
@@ -1,23 +1,23 @@
 package mb.lib.report.model
 
-import com.fasterxml.jackson.annotation.JsonGetter
-import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonSetter
+import com.fasterxml.jackson.annotation.JsonValue
+import com.fasterxml.jackson.databind.node.ObjectNode
 import mb.api.model.io.JsonKeys
 import org.veupathdb.lib.blast.BlastFormatter
 import org.veupathdb.lib.hash_id.HashID
+import org.veupathdb.lib.jackson.Json
 
-@JsonInclude(JsonInclude.Include.NON_EMPTY)
 data class ReportPayload(
-  @get:JsonGetter(JsonKeys.JobID)
-  @set:JsonSetter(JsonKeys.JobID)
-  var jobID: HashID? = null,
+  @set:JsonSetter(JsonKeys.JobID)    var jobID:    HashID?         = null,
+  @set:JsonSetter(JsonKeys.ReportID) var reportID: HashID?         = null,
+  @set:JsonSetter(JsonKeys.Config)   var config:   BlastFormatter? = null,
+) {
 
-  @get:JsonGetter(JsonKeys.ReportID)
-  @set:JsonSetter(JsonKeys.ReportID)
-  var reportID: HashID? = null,
-
-  @get:JsonGetter(JsonKeys.Config)
-  @set:JsonSetter(JsonKeys.Config)
-  var config: BlastFormatter? = null
-)
+  @JsonValue
+  fun toJson() = Json.new<ObjectNode> {
+    jobID?.run { put(JsonKeys.JobID, string) }
+    reportID?.run { put(JsonKeys.ReportID, string) }
+    config?.run { set<ObjectNode>(JsonKeys.Config, toJSON().toJSON()) }
+  }
+}
