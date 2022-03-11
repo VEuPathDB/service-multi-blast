f592e4af1b646faee42d0a8f583475c220064d07
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-03-11 08:34:21 -0500

    update dependency

--- rest-service/build.gradle.kts
+++ rest-service/build.gradle.kts
@@ -134,6 +134,7 @@ dependencies {
 
   implementation("org.veupathdb.lib:lib-prometheus-stats:1.1.0")
   implementation("org.veupathdb.lib:jvm-fireworq:1.0.3")
+  implementation("org.veupathdb.lib:jackson-singleton:2.1.0")
 
   // Unit Testing
   testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
