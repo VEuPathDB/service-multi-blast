7e5e0b2f8778e5f88c86660dbe5bbe29dbb1ef68
Elizabeth <foxcapade@gmail.com>
2022-04-05 13:46:17 -0400

    cleanup dead queue methods

--- rest-service/src/main/kotlin/mb/lib/queue/QueueManager.kt
+++ rest-service/src/main/kotlin/mb/lib/queue/QueueManager.kt
@@ -11,25 +11,9 @@ abstract class QueueManager
 
   fun getFailedJobs() = fireworq.getFailed()
 
-  fun getJobStatus(jobID: Int): JobStatus {
-    val job = fireworq.getJob(jobID) ?:
-      return if (jobInFailList(jobID))
-        JobStatus.Errored
-      else
-        JobStatus.Completed
-
-    return JobStatus.unsafeFromString(job.status)
-  }
-
   fun grabbedJobs() = fireworq.getGrabbed()
 
   fun queueSize() = fireworq.getWaiting().size + fireworq.getGrabbed().size
-
-  fun jobInFailList(jobID: Int) = fireworq.jobInFailList(jobID)
-
-  fun deleteJobFailure(failID: Int) {
-    fireworq.deleteJobFailure(failID)
-  }
 
   fun submitNewJob(category: String, req: CreateRequest<*>) =
     fireworq.submitJob(category, req.toInternal()).jobID
