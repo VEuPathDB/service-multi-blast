0bd4290ec80cb922821e3e2568c85f9e39db2ac3
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-04-05 14:10:22 -0400

    busy-wait for report file.  #102

--- blast/formatter/server/internal/server/run-report-ep.go
+++ blast/formatter/server/internal/server/run-report-ep.go
@@ -8,6 +8,7 @@ import (
 	"path/filepath"
 	"server/internal/model"
 	"server/internal/xfiles"
+	"time"
 
 	"github.com/francoispqt/gojay"
 	"github.com/sirupsen/logrus"
@@ -102,13 +103,22 @@ func runReport(job *api.JobPayload, log *logrus.Entry) midl.Response {
 }
 
 // zipDir zips the contents of the given directory.
-func zipDir(path string) error {
-	if matches, err := filepath.Glob(filepath.Join(path, "*")); err != nil {
-		return err
-	} else {
-		if err := xfiles.ZipFiles(path, reportExportName, matches); err != nil {
+func zipDir(path string) (err error) {
+	var matches []string
+
+	// Loop to wait for the report file if we get here before it's copied into the
+	// workspace directory.
+	for len(matches) == 0 {
+		if matches, err = filepath.Glob(filepath.Join(path, "*")); err != nil {
 			return err
+		} else {
+			if err := xfiles.ZipFiles(path, reportExportName, matches); err != nil {
+				return err
+			}
 		}
+
+		// Wait a tenth of a second before checking again.
+		time.Sleep(100 * time.Millisecond)
 	}
 
 	return nil
