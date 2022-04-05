4b7c54429b848569b10d6579f8429ccbdce420ad
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-04-05 11:02:50 -0400

    add safety check looking for completion flags in report workspace before attempting to run the report job

--- blast/formatter/server/internal/xfiles/checks.go
+++ blast/formatter/server/internal/xfiles/checks.go
@@ -3,6 +3,7 @@ package xfiles
 import (
 	"errors"
 	"os"
+	"path/filepath"
 )
 
 func FileExists(path string) (bool, error) {
@@ -14,5 +15,29 @@ func FileExists(path string) (bool, error) {
 		}
 
 		return false, err
+	}
+}
+
+// CompletionFlagExists tests whether the given report workspace directory
+// contains either a successful or failed completion flag file.
+func CompletionFlagExists(dir string) (bool, error) {
+	// Check for the successful completion flag.
+	if exists, err := FileExists(filepath.Join(dir, completeFileName)); err != nil {
+		// File stat failed.
+		return false, err
+	} else if exists {
+		// If the successful completion flag exists, return true.
+		return true, nil
+	}
+
+	// The successful completion flag does not exist.
+
+	// Check for the failed completion flag.
+	if exists, err := FileExists(filepath.Join(dir, failedFileName)); err != nil {
+		// File stat failed.
+		return false, err
+	} else {
+		// Return whether the failed completion flag exists.
+		return exists, nil
 	}
 }
