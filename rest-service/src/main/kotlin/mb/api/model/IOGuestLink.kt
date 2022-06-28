c0588c786c8e851a9b9c90ad948b761998d993a0
Elizabeth <foxcapade@gmail.com>
2022-06-28 09:17:41 -0400

    add property name to json property annotation

--- rest-service/src/main/kotlin/mb/api/model/IOGuestLink.kt
+++ rest-service/src/main/kotlin/mb/api/model/IOGuestLink.kt
@@ -2,4 +2,4 @@ package mb.api.model
 
 import com.fasterxml.jackson.annotation.JsonProperty
 
-data class IOGuestLink(@JsonProperty var guestID: Long)
+data class IOGuestLink(@JsonProperty("guestID") var guestID: Long)
