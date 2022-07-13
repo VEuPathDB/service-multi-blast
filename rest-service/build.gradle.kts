752fb7813933fa6343b76956af35663a6db0fdf2
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-07-13 15:35:56 -0400

    updates

--- rest-service/build.gradle.kts
+++ rest-service/build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
   java
   id("org.veupathdb.lib.gradle.container.container-utils") version "4.0.0"
   id("com.github.johnrengelman.shadow") version "7.1.2"
-  kotlin("jvm") version "1.6.21"
+  kotlin("jvm") version "1.7.0"
 }
 
 // Load Props
