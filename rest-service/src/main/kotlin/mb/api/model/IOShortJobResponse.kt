7121071da991a308e5887498066757ee98d62cfa
Elizabeth <foxcapade@gmail.com>
2022-07-21 11:01:57 -0400

    these properties are never null

--- rest-service/src/main/kotlin/mb/api/model/IOShortJobResponse.kt
+++ rest-service/src/main/kotlin/mb/api/model/IOShortJobResponse.kt
@@ -3,42 +3,38 @@ package mb.api.model
 import com.fasterxml.jackson.annotation.JsonGetter
 import com.fasterxml.jackson.annotation.JsonInclude
 import com.fasterxml.jackson.annotation.JsonInclude.Include.NON_NULL
-import com.fasterxml.jackson.annotation.JsonProperty
 import mb.api.model.io.JsonKeys
 import mb.lib.model.JobStatus
 
 @JsonInclude(NON_NULL)
 open class IOShortJobResponse(
   @get:JsonGetter(JsonKeys.ID)
-  val id: String? = null,
+  val id: String,
 
   @get:JsonGetter(JsonKeys.Description)
-  val description: String? = null,
+  val description: String?,
 
   @get:JsonGetter(JsonKeys.Status)
-  val status: JobStatus? = null,
+  val status: JobStatus,
 
   @get:JsonGetter(JsonKeys.Created)
-  val created: String? = null,
-
-  @get:JsonGetter(JsonKeys.Expires)
-  val expires: String? = null,
+  val created: String?,
 
   @get:JsonGetter(JsonKeys.MaxResultSize)
-  val maxResultSize: Long? = null,
+  val maxResultSize: Long,
 
   @get:JsonGetter(JsonKeys.ParentJobs)
-  val parentJobs: Array<IOJobLink>? = null,
+  val parentJobs: Array<IOJobLink>?,
 
   @get:JsonGetter(JsonKeys.ChildJobs)
-  val childJobs: Array<IOJobLink>? = null,
+  val childJobs: Array<IOJobLink>?,
 
   @get:JsonGetter(JsonKeys.IsPrimary)
   val isPrimary: Boolean,
 
   @get:JsonGetter(JsonKeys.Site)
-  val site: String? = null,
+  val site: String,
 
   @get:JsonGetter(JsonKeys.Targets)
-  val targets: Array<IOJobTarget>? = null,
+  val targets: Array<IOJobTarget>?,
 )
