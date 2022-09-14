6b737d76270d2b848fdac2ed5035259d2d731872
Elizabeth <foxcapade@gmail.com>
2022-09-14 14:19:07 -0400

    updates

---
+++ query-service/src/main/kotlin/mblast/Executor.kt
@@ -0,0 +1,14 @@
+package mblast
+
+import org.veupathdb.lib.blast.blastn.BlastN
+import org.veupathdb.lib.compute.platform.job.JobContext
+import org.veupathdb.lib.compute.platform.job.JobExecutor
+import org.veupathdb.lib.compute.platform.job.JobResult
+
+class Executor : JobExecutor {
+  override fun execute(ctx: JobContext): JobResult {
+    
+
+    TODO()
+  }
+}
