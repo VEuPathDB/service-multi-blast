1060371d02a67eed528a14548f042b35f34fc8c3
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-10-16 10:54:09 -0400

    updates

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,7 +16,9 @@ pluginManagement {
 
 include("query-service", "report-service")
 
-//include("common:blast-db-path")
-//findProject(":common:blast-db-path")?.name = "blast-db-path"
+include("common:blast-types")
+findProject(":common:blast-types")!!.name = "blast-types"
+include("common:mblast-utils")
+findProject(":common:mblast-utils")!!.name = "mblast-utils"
 include("common:query-parser")
-findProject(":common:query-parser")?.name = "query-parser"
+findProject(":common:query-parser")!!.name = "query-parser"
