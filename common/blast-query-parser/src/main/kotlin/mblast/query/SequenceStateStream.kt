a04aea2bc26ab81786bc7c73ec9bdff0ade87c14
Elizabeth <foxcapade@gmail.com>
2022-10-15 22:09:43 -0400

    names and docs

--- common/blast-query-parser/src/main/kotlin/mblast/query/SequenceStateStream.kt
+++ common/blast-query-parser/src/main/kotlin/mblast/query/SequenceStateStream.kt
@@ -4,8 +4,20 @@ import mblast.query.util.EOF
 import mblast.query.util.NUL
 import mblast.util.io.InStream
 
+/**
+ * BLAST Query State Machine Input Stream
+ *
+ * An input stream implementation base backed by [HeadlessSequenceStateMachine].
+ *
+ * @author Elizabeth Paige Harper - https://github.com/foxcapades
+ * @since  1.0.0
+ */
 abstract class SequenceStateStream : HeadlessSequenceStateMachine(), InStream {
 
+  /**
+   * Returns the next byte in the sequence or -1 if the end of the stream has
+   * been reached.
+   */
   override fun read(): Int {
     // If the current character is the null character, then we haven't read any
     // data yet, meaning we are at the beginning of the input.
