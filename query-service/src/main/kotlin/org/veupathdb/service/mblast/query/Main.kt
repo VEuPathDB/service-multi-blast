c8df5dd8e571b4b7c569e6ef880e6af49a9e4784
Elizabeth <foxcapade@gmail.com>
2022-10-17 14:53:13 -0400

    add option

--- query-service/src/main/kotlin/org/veupathdb/service/mblast/query/Main.kt
+++ query-service/src/main/kotlin/org/veupathdb/service/mblast/query/Main.kt
@@ -1,11 +1,10 @@
-@file:JvmName("Main")
-
 package org.veupathdb.service.mblast.query
 
 import org.veupathdb.lib.compute.platform.AsyncPlatform
 import org.veupathdb.lib.compute.platform.config.*
 import org.veupathdb.lib.container.jaxrs.config.Options
 import org.veupathdb.lib.container.jaxrs.server.Server
+import org.veupathdb.service.mblast.query.jobs.ExecutorFactory
 
 class Main : Server() {
 
@@ -33,9 +32,10 @@ class Main : Server() {
         .accessToken(ServiceOptions.s3AccessToken)
         .secretKey(ServiceOptions.s3SecretKey)
         .port(ServiceOptions.s3Port)
+        .https(ServiceOptions.s3UseHttps)
         .build())
       .addQueue(AsyncQueueConfig.builder()
-        .id(ServiceOptions.queueNamePrimary)
+        .id(ServiceOptions.parentJobQueueName)
         .username(ServiceOptions.jobQueueUsername)
         .password(ServiceOptions.jobQueuePassword)
         .host(ServiceOptions.jobQueueHost)
@@ -43,7 +43,7 @@ class Main : Server() {
         .workers(ServiceOptions.jobQueueWorkers)
         .build())
       .addQueue(AsyncQueueConfig.builder()
-        .id(ServiceOptions.queueNameSecondary)
+        .id(ServiceOptions.childJobQueueName)
         .username(ServiceOptions.jobQueueUsername)
         .password(ServiceOptions.jobQueuePassword)
         .host(ServiceOptions.jobQueueHost)
@@ -58,6 +58,7 @@ class Main : Server() {
     fun main(args: Array<String>) {
       val server = Main()
 
+      server.enableAccountDB()
       server.enableUserDB()
 
       server.start(args)
--- query-service/src/main/kotlin/org/veupathdb/service/mblast/query/ServiceOptions.kt
+++ query-service/src/main/kotlin/org/veupathdb/service/mblast/query/ServiceOptions.kt
@@ -23,18 +23,40 @@ object ServiceOptions : Options() {
   val s3AccessToken = NBEnv.require("S3_ACCESS_TOKEN")
   val s3SecretKey   = NBEnv.require("S3_SECRET_KEY")
   val s3Port        = NBEnv.require("S3_PORT") { it.toInt() }
+  val s3UseHttps    = NBEnv.require("S3_USE_HTTPS") { it.toBoolean() }
 
   val jobCacheTimeoutDays = NBEnv.require("JOB_CACHE_TIMEOUT_DAYS") { it.toInt() }
 
-  val siteBuild = NBEnv.require("SITE_BUILD")
+  val siteBuildDir = NBEnv.require("SITE_BUILD_DIR")
 
-  val maxQueriesPerJob   = NBEnv.require("MAX_QUERIES_PER_JOB") { it.toInt() }
+  val blastDBRootDir = NBEnv.require("BLAST_DB_ROOT_DIR")
+
+  /**
+   * Maximum number of sequences in a single mblast job.
+   */
+  val maxQueriesPerJob   = NBEnv.require("MAX_SEQUENCES_PER_JOB") { it.toInt() }
+
+  /**
+   * Maximum number of results allowed per job.
+   */
   val maxResultsPerQuery = NBEnv.require("MAX_RESULTS_PER_QUERY") { it.toInt() }
+
+  /**
+   * Maximum size (in bytes) of an uploaded query.
+   */
   val maxInputQuerySize  = NBEnv.require("MAX_INPUT_QUERY_SIZE") { it.toInt() }
+
+  /**
+   * Maximum size (in bytes) of a single nucleotide sequence.
+   */
   val maxNASeqSize       = NBEnv.require("MAX_NA_SEQ_SIZE") { it.toInt() }
+
+  /**
+   * Maximum size (in bytes) of a single protein sequence.
+   */
   val maxAASeqSize       = NBEnv.require("MAX_AA_SEQ_SIZE") { it.toInt() }
 
-  val queueNamePrimary   = NBEnv.require("QUEUES_QUERY_NAME_1")
-  val queueNameSecondary = NBEnv.require("QUEUES_QUERY_NAME_2")
+  val parentJobQueueName   = NBEnv.require("QUERY_SERVICE_QUEUE_NAME_1")
+  val childJobQueueName = NBEnv.require("QUERY_SERVICE_QUEUE_NAME_2")
 
 }
