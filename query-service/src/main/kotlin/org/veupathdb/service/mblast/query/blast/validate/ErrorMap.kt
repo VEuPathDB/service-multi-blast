976403762aecde3232f130dd756cb0bc71a5f899
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-10-18 14:24:14 -0400

    comments

--- query-service/src/main/kotlin/org/veupathdb/service/mblast/query/blast/validate/ErrorMap.kt
+++ query-service/src/main/kotlin/org/veupathdb/service/mblast/query/blast/validate/ErrorMap.kt
@@ -1,5 +1,11 @@
 package org.veupathdb.service.mblast.query.blast.validate
 
+/**
+ * Validation Error Map
+ *
+ * A wrapper around `Map<String, List<String>>` with convenience methods for
+ * appending errors.
+ */
 class ErrorMap(private val raw: MutableMap<String, MutableList<String>>) {
   fun blastField(key: String, error: String) {
     raw.computeIfAbsent("blastConfig.$key") { ArrayList(1) }
