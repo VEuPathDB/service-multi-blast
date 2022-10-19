c2fc7fd72f62b5f4ad1a23d5732963db81d2972c
Elizabeth <foxcapade@gmail.com>
2022-10-19 13:36:24 -0400

    move buildSrc

--- lib-mblast-gradle/build.gradle.kts
+++ lib-mblast-gradle/build.gradle.kts
@@ -2,10 +2,14 @@ plugins {
   kotlin("jvm") version "1.7.20"
 }
 
+group = "mblast"
+version = '0'
+
 repositories {
   mavenCentral()
 }
 
 dependencies {
+  implementation(gradleApi())
   implementation("com.hierynomus:sshj:0.34.0")
 }
