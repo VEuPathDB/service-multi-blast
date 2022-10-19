fa8fe76e94c6e48604b1d558d40d31752864b66e
Elizabeth <foxcapade@gmail.com>
2022-10-19 11:37:07 -0400

    builds

--- lib-blast-types/build.gradle.kts
+++ lib-blast-types/build.gradle.kts
@@ -1,7 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
-  kotlin("jvm") version "1.7.20"
+  kotlin("jvm")
   id("org.jetbrains.dokka") version "1.7.10"
   `maven-publish`
 }
