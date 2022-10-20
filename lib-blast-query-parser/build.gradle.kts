9c3241270a7e1b7963cbbe65d3b97fef0775d4a9
Elizabeth <foxcapade@gmail.com>
2022-10-20 15:03:21 -0400

    updates

--- lib-blast-query-parser/build.gradle.kts
+++ lib-blast-query-parser/build.gradle.kts
@@ -1,41 +1,26 @@
 plugins {
   kotlin("jvm")
   id("org.jetbrains.dokka") version "1.7.10"
-  `maven-publish`
 }
 
-group = "org.veupathdb.lib"
-version = "1.0-SNAPSHOT"
-
 dependencies {
-  implementation("org.veupathdb.lib.mblast:mblast-utils:1.0-SNAPSHOT") { isChanging = true }
+  implementation(project(":lib-mblast-utils"))
 }
 
 java {
-  targetCompatibility = JavaVersion.VERSION_17
-  sourceCompatibility = JavaVersion.VERSION_17
+  targetCompatibility = JavaVersion.VERSION_1_8
+  sourceCompatibility = JavaVersion.VERSION_1_8
 
   withSourcesJar()
   withJavadocJar()
-}
-
-tasks.jar {
-  manifest {
-    attributes["Implementation-Title"] = project.name
-    attributes["Implementation-Version"] = project.version
-  }
 }
 
 tasks.dokkaHtml {
   outputDirectory.set(rootDir.resolve("docs/dokka/mblast-utils/$version"))
 }
 
-tasks.dokkaJavadoc {
-  outputDirectory.set(rootDir.resolve("docs/javadoc/mblast-utils/$version"))
-}
-
 tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
   kotlinOptions {
-    jvmTarget = "17"
+    jvmTarget = "1.8"
   }
 }
