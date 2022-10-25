7c0ac15d29d59cca474c27436f3b36fd860a8a8a
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-10-25 11:46:27 -0400

    updates

---
+++ service-query/src/main/kotlin/org/veupathdb/service/mblast/query/Const.kt
@@ -0,0 +1,14 @@
+package org.veupathdb.service.mblast.query
+
+object Const {
+  const val ConfigFileName = "config.json"
+  const val QueryFileName  = "query.txt"
+  const val StdErrFileName = "stderr.txt"
+  const val ReportFileName = "report.asn1"
+
+  const val ThreadContextJobID = "JOB_ID"
+
+  const val MaxSummaryLength = 512
+
+  const val QueryTempDir = "/tmp"
+}
