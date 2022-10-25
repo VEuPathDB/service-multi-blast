7bed9867894810bce1ff31068396c6185af772f7
Elizabeth <foxcapade@gmail.com>
2022-10-25 17:51:54 -0400

    updates

--- service-query/src/main/kotlin/org/veupathdb/service/mblast/query/jobs/ExecutorFactory.kt
+++ service-query/src/main/kotlin/org/veupathdb/service/mblast/query/jobs/ExecutorFactory.kt
@@ -1,9 +1,8 @@
-package org.veupathdb.service.mblast.query
+package org.veupathdb.service.mblast.query.jobs
 
 import org.veupathdb.lib.compute.platform.job.JobExecutorContext
 import org.veupathdb.lib.compute.platform.job.JobExecutorFactory
 
 class ExecutorFactory : JobExecutorFactory {
-
-  override fun newJobExecutor(ctx: JobExecutorContext) = Executor()
+  override fun newJobExecutor(ctx: JobExecutorContext) = BlastExecutor()
 }
