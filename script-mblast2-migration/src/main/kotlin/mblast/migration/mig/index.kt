9cb7d6eac0a955543eb5b3f3d3f399edd3f70565
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-10-25 16:55:05 -0400

    updates

--- script-mblast2-migration/src/main/kotlin/mblast/migration/mig/index.kt
+++ script-mblast2-migration/src/main/kotlin/mblast/migration/mig/index.kt
@@ -6,14 +6,12 @@ fun RunMigration(dataSource: DataSource) {
   dataSource.connection.use { con ->
     con.autoCommit = false
 
-
     _migrateQueryJobs(con)
     con.commit()
 
     _migrateQueryTargets(con)
     _migrateQueryUserLinks(con)
     _migrateQuerySubJobs(con)
-
 
     println("committing")
     con.commit()
