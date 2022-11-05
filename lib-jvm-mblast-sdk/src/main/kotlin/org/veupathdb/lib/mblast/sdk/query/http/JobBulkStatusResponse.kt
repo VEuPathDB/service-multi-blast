66cb6e14e7022da3e4d2d643a69a72e7475671a2
Elizabeth <foxcapade@gmail.com>
2022-11-05 18:34:16 -0400

    updates

--- lib-jvm-mblast-sdk/src/main/kotlin/org/veupathdb/lib/mblast/sdk/query/http/JobBulkStatusResponse.kt
+++
@@ -1,8 +0,0 @@
-package org.veupathdb.lib.mblast.sdk.query.http
-
-import org.veupathdb.lib.hash_id.HashID
-
-class JobBulkStatusResponse : HashMap<HashID, JobStatus> {
-  constructor() : super()
-  constructor(x: Int) : super(x)
-}
