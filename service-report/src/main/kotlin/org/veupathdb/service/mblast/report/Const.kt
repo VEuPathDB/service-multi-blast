fad9c690a663d13d2fcaa755ba38b7e44babc05e
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-11-06 15:05:17 -0500

    updates

---
+++ service-report/src/main/kotlin/org/veupathdb/service/mblast/report/Const.kt
@@ -0,0 +1,17 @@
+package org.veupathdb.service.mblast.report
+
+object Const {
+  const val ARCHIVE_FILE = "input.asn1"
+
+  const val CONFIG_FILE = "config.json"
+
+  const val MAX_USER_SUMMARY_LENGTH = 512
+
+  const val OUTPUT_FILE_PREFIX = "report"
+
+  const val OUTPUT_ZIP_NAME = "report.zip"
+
+  const val STD_ERR_FILE_NAME = "stderr.txt"
+
+  const val THREAD_CONTEXT_JOB_ID = "JOB_ID"
+}
