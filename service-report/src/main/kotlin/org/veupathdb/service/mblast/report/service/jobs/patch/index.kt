48abe5030f6aa625a58a3c93172e1c342f2b41fa
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-11-06 16:04:28 -0500

    docs

--- service-report/src/main/kotlin/org/veupathdb/service/mblast/report/service/jobs/patch/index.kt
+++ service-report/src/main/kotlin/org/veupathdb/service/mblast/report/service/jobs/patch/index.kt
@@ -8,26 +8,44 @@ import org.veupathdb.service.mblast.report.db.ReportDB
 import org.veupathdb.service.mblast.report.generated.model.ReportJobPatchRequest
 import org.veupathdb.service.mblast.report.model.UserMeta
 
+/**
+ * Updates a user to job link with the new meta provided in the PATCH request.
+ *
+ * Values in the PATCH request that are `null` will not be updated.
+ *
+ * @param reportJobID ID of the job the link to update targets.
+ *
+ * @param userID ID of the user whose link should be updated.
+ *
+ * @param entity PATCH request payload.
+ */
 fun PatchUserJob(reportJobID: HashID, userID: Long, entity: ReportJobPatchRequest) {
+  // Verify that the request body actually contained something.
   if (!entity.hasSomethingToSet())
     return
 
+  // Validate the request body.
   entity.validate()
 
+  // Lookup the user-to-job link.
   val job = ReportDB.selectReportJob(reportJobID, userID)
     ?: throw NotFoundException()
 
+  // Create the new metadata to set, defaulting null values back to their
+  // currently saved values (which may be null).
   val newMeta = UserMeta(
     userID,
     entity.userMeta.summary ?: job.userSummary,
     entity.userMeta.description ?: job.userDescription,
   )
 
-  ReportDB.withTransaction {
-    it.updateUserMeta(reportJobID, newMeta)
-  }
+  // Update the user link in the database.
+  ReportDB.withTransaction { it.updateUserMeta(reportJobID, newMeta) }
 }
 
+/**
+ * Tests whether the request body contains values to set on the user link.
+ */
 private fun ReportJobPatchRequest.hasSomethingToSet() =
   when {
     userMeta == null                                         -> false
