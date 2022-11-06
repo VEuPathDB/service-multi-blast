773ffe6e892373a1e84fb755613dbe6853a7c347
Elizabeth <foxcapade@gmail.com>
2022-11-06 16:07:26 -0500

    docs

--- service-report/src/main/kotlin/org/veupathdb/service/mblast/report/service/cache/files/list-files.kt
+++ service-report/src/main/kotlin/org/veupathdb/service/mblast/report/service/cache/files/list-files.kt
@@ -7,6 +7,17 @@ import org.veupathdb.lib.hash_id.HashID
 import org.veupathdb.service.mblast.report.generated.model.FileEntry
 import org.veupathdb.service.mblast.report.generated.model.FileEntryImpl
 
+/**
+ * Returns a list of result files present in the cache for a target report job.
+ *
+ * @param jobID ID of the target report job whose result files should be listed.
+ *
+ * @return A list of zero or more files present in the cache for the target job.
+ *
+ * @throws NotFoundException If the target job does not exist.
+ *
+ * @throws ForbiddenException If the target job is not in a finished state.
+ */
 fun ListJobFiles(jobID: HashID): List<FileEntry> {
   val job = AsyncPlatform.getJob(jobID)
     ?: throw NotFoundException()
