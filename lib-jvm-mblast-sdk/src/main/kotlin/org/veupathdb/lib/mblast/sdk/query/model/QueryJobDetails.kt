7e67d0a5aea7ef9e7f1e01b29abbd8719e61f19d
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2022-11-12 10:12:29 -0500

    updates

--- lib-jvm-mblast-sdk/src/main/kotlin/org/veupathdb/lib/mblast/sdk/query/model/QueryJobDetails.kt
+++ lib-jvm-mblast-sdk/src/main/kotlin/org/veupathdb/lib/mblast/sdk/query/model/QueryJobDetails.kt
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.annotation.JsonProperty
 import org.veupathdb.lib.hash_id.HashID
 import org.veupathdb.lib.mblast.sdk.common.model.JobStatus
 import org.veupathdb.lib.mblast.sdk.query.blast.BlastQueryConfig
+import java.time.OffsetDateTime
 
 data class QueryJobDetails(
   @JsonProperty("queryJobID")
@@ -17,6 +18,9 @@ data class QueryJobDetails(
 
   @JsonProperty("blastConfig")
   val blastConfig: BlastQueryConfig,
+
+  @JsonProperty("createdOn")
+  val createdOn: OffsetDateTime,
 
   @JsonProperty("userMeta")
   val userMeta: QueryJobUserMeta?,
