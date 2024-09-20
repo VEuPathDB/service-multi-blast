9b0965b84520a95dc10fe7a2a6b63e1fe51c5568
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-09-20 14:39:34 -0400

    fix config serialization

--- rest-service/src/main/kotlin/mb/lib/query/model/BlastConfig.kt
+++ rest-service/src/main/kotlin/mb/lib/query/model/BlastConfig.kt
@@ -1,9 +1,8 @@
 package mb.lib.query.model
 
 import org.veupathdb.lib.blast.BlastConfig
-import org.veupathdb.lib.jackson.Json
 
 @JvmInline
 value class BlastConfig(val config: BlastConfig) : JobConfig {
-  override fun toJson() = Json.convert(config)
+  override fun toJson() = config.toJSON(true).toJSON()
 }
